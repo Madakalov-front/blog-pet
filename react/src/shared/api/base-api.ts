5f93df80f07e78de2367d2225b8e3db50c7718cb
Egor Madakalov <gpt.e.madakalov@gmail.com>
2025-08-17 17:35:29 +0500

    update api-url

--- react/src/shared/api/base-api.ts
+++ react/src/shared/api/base-api.ts
@@ -4,14 +4,17 @@ export const baseApi = createApi({
     reducerPath: "api",
     tagTypes: ["Post", "Comment", "User"],
     baseQuery: fetchBaseQuery({
-        baseUrl: 'https://blog-pet.onrender.com',
+        baseUrl: "/api",
         prepareHeaders: (headers) => {
             const token = localStorage.getItem("token");
             if (token) {
                 headers.set("Authorization", `Bearer ${JSON.parse(token)}`);
             }
-            headers.set('Access-Control-Allow-Origin', 'https://blog-pet-seven.vercel.app');
-      headers.set('Access-Control-Allow-Credentials', 'true');
+            headers.set(
+                "Access-Control-Allow-Origin",
+                "https://blog-pet-seven.vercel.app"
+            );
+            headers.set("Access-Control-Allow-Credentials", "true");
             return headers;
         },
     }),
